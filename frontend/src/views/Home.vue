973576319343539dde87d29967577a5a4a1f909d
DESKTOP\multicampus <ehdgns5212@gmail.com>
2021-07-27 17:26:58 +0900

    #S05P12A402-273 FE [수정] 모달 구조 설정

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,54 @@
 <template>
-    <div>
-        <h1>This is Homepage</h1>
-        <button>로그인</button>
-        <button>회원가입</button>
-        <v-btn elevation="2">뷰버튼</v-btn>
-    </div>
+  <v-row justify="center">
+
+    <v-dialog
+      v-model="isLoginViewd"
+      persistent
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Open Dialog
+        </v-btn>
+      </template>
+      <login @close="isLoginViewd=false" @signup="isSigUpViewd=true;isLoginViewd=false"/>
+    </v-dialog>
+    <v-dialog
+      v-model="isLoginViewd"
+      persistent
+      max-width="600px"
+    >
+      <login @close="isLoginViewd=false" />
+    </v-dialog>
+    <v-dialog
+      v-model="isSignUpViewd"
+      persistent
+      max-width="600px"
+    > <sign-up @close="isSignUpViewd=false" />
+    </v-dialog>
+  </v-row>
 </template>
 
 <script>
+import Login from "@/components/home/Login.vue";
+import SignUp from "@/components/home/SignUp.vue";
 export default {
-
-}
+    components: {
+        Login,
+        SignUp,
+    },
+    data() {
+        return {     
+            isLoginViewd: false,
+            isSignUpViewd: false,
+        }
+    },
+  }
 </script>
 
 <style>

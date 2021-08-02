eae9e77acc598b98b4a40bfebbd8366c163262bd
DESKTOP-KVCQHCD\multicampus <seankim95@naver.com>
2021-08-02 14:23:10 +0900

    S05P13A402-116 FE [추가] 사진 수정 버튼 추가

--- frontend/src/components/mypage/ProfileImage.vue
+++ frontend/src/components/mypage/ProfileImage.vue
@@ -1,19 +1,32 @@
 <template>
-    <div class="profile-image-box">
+    <div class="profile-image-whole">
+        <div class="profile-image-box">
+            <img class="profile-image" src="images/mypage/rabbit.jpg" alt="logo.png">
+        </div>
         <!-- <img class="profile-image" src="images/mypage/seunghee.jpg" alt="logo.png"> -->
         <!-- <img class="profile-image" src="images/mypage/samsung.png" alt="logo.png"> -->
         <!-- <img class="profile-image" src="images/mypage/admin.png" alt="logo.png"> -->
-        <img class="profile-image" src="images/mypage/rabbit.jpg" alt="logo.png">
+        <v-btn class="imageEdit" v-if="isMe ">사진 편집</v-btn>
     </div>
 </template>
 
 <script>
 export default {
-
+    computed: {
+        isMe: function() {
+            return (this.$store.state.userType == this.$store.state.mypage.profileType) &&
+            (this.$store.state.userId == this.$store.state.mypage.profileId)
+        },
+    }
 }
 </script>
 
 <style>
+.profile-image-whole {
+    height: 300px;
+    width: 300px;
+}
+
 .profile-image-box {
     position: relative;
     height: 300px;
@@ -21,6 +34,13 @@ export default {
     left: 80px;
     border-radius: 70%;
     overflow: hidden;
+}
+
+.imageEdit {
+    border: black solid 1px;
+    position: relative;
+    top: -20px;
+    left: 270px;
 }
 
 .profile-image {

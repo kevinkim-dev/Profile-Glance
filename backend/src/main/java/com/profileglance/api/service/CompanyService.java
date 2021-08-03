89b83ff81e114e43a5b5ac75ac47d2386e759c96
심재우 <jw_shim@naver.com>
2021-08-03 18:33:01 +0900

    #S05P13A402-51 BE [추가] 기업_유저_좋아요
    CompanyController 수정
    CompanyService 수정
    CompanyServiceImpl 수정
    UserLikeRepository 추가

--- backend/src/main/java/com/profileglance/api/service/CompanyService.java
+++ backend/src/main/java/com/profileglance/api/service/CompanyService.java
@@ -1,9 +1,14 @@
 package com.profileglance.api.service;
 
 import com.profileglance.db.entity.Company;
+import com.profileglance.db.entity.User;
+
+import java.util.List;
 
 public interface CompanyService {
 
     Company createCompany(Company company);
 
+    List<User> userLikeListByCompany(String companyId);
+    boolean isHitLike(String userEmail,String companyId);
 }

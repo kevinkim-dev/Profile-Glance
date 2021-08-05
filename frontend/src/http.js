b6918b3e231a4c2a107a9eadc1194f2050c190b0
DESKTOP-KVCQHCD\multicampus <seankim95@naver.com>
2021-08-05 16:11:19 +0900

    #S05P13A402-104 FE [수정] 기업회원 회원 정보
    배포용 서버 주소 주석으로 추가

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
+  // baseURL: "http://52.79.113.173:8888/",
   baseURL: "http://localhost:8888/",
   headers: {
     "Content-type": "application/json",

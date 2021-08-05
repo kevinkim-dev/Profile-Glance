eabd7ad18743ec1d443170e9663ab0393975de66
심재우 <jw_shim@naver.com>
2021-08-05 11:27:49 +0900

    #S05P13A402-51 BE [추가] 기업회원 정보보기

---
+++ backend/src/main/java/com/profileglance/api/response/CompanyMypageGetRes.java
@@ -0,0 +1,35 @@
+package com.profileglance.api.response;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel("Mypage_Company")
+public class CompanyMypageGetRes {
+
+    @ApiModelProperty(name="기업아이디")
+    String companyId;
+
+    @ApiModelProperty(name="기업 이름")
+    String companyName;
+
+    @ApiModelProperty(name="기업 이메일")
+    String companyEmail;
+
+    @ApiModelProperty(name="기업 연락처")
+    String companyPhone;
+
+    @ApiModelProperty(name="기업 로고 사진")
+    String companyImg;
+
+    public CompanyMypageGetRes(String companyId, String companyName, String companyEmail, String companyPhone, String companyImg) {
+        this.companyId = companyId;
+        this.companyName = companyName;
+        this.companyEmail = companyEmail;
+        this.companyPhone = companyPhone;
+        this.companyImg = companyImg;
+    }
+}
